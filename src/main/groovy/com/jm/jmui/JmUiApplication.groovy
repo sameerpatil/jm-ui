a631c35441abb04579b832640970f675856d44d4
Sameer Patil <sameerpatil@yahoo.com>
2017-09-30 22:19:24 +0530

    Initial commit

---
+++ src/main/groovy/com/jm/jmui/JmUiApplication.groovy
@@ -0,0 +1,12 @@
+package com.jm.jmui
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+//@SpringBootApplication
+class JmUiApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run JmUiApplication, args
+	}
+}
---
+++ src/main/java/com/janmudra/jmui/JmUiApplication.java
@@ -0,0 +1,12 @@
+package com.janmudra.jmui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( JmUiApplication.class, args);
+	}
+}
